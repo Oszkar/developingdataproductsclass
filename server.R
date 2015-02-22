53658770f357c4af1b9a241f9a7dfea70d3d4b7e
Oszkar Jozsa <jozsa.oszkar@gmail.com>
2015-02-22 21:48:52 +0100

    More interactivity, second panel for box office

--- server.R
+++ server.R
@@ -12,23 +12,44 @@ movies[,15] <- as.numeric(movies[,15])
 movies[,16] <- as.numeric(movies[,16])
 # imdb votes
 movies[,17] <- as.numeric(gsub(",", "", movies[,17]))
-size <- (movies[,17] - min(movies[,17])) / max(movies[,17])
-size <- round(size * 20 + 3)
-movies[,35] <- size
 # tomatometer
 movies[,20] <- as.numeric(movies[,20])
 # tomato rating
 movies[,22] <- as.numeric(movies[,22])
 # tomato user rating
 movies[,28] <- as.numeric(movies[,28])
+# number of tomato user rating
+movies[,29] <- as.numeric(gsub(",", "", movies[,29]))
+# insert 0 instead of NA
+movies[is.na(movies[,29]),29] <- 0
+# decade
+movies[,35] <- movies$Year - movies$Year %% 10
+# add a name to the decade variable too
+colnames(movies)[35] = "decade"
+
+# second data frame only for the movies that contain Box Office data
+movieswithBO <- movies[-which(movies$BoxOffice == "N/A"),]
+
+convertDollarToNumeric <- function(item) {
+  # first, remove dollar sign
+  item <- sub('\\$','',item) 
+  # check the last character
+  if(substr(item, nchar(item), nchar(item)) == "M") {
+    # then millions
+    item <- as.numeric(substr(item, 1, nchar(item)-1)) * 1000000
+  } else {
+    # else thousands
+    item <- as.numeric(substr(item, 1, nchar(item)-1)) * 1000
+  }
+  return(item)
+}
+
+movieswithBO[,35] <- as.numeric(sapply(movieswithBO$BoxOffice, convertDollarToNumeric))
+
+colnames(movieswithBO)[35] = "Earnings"
+movieswithBO$Earnings.html.tooltip <- movieswithBO$Title
 
 shinyServer(function(input, output) {
-  
-  datasetInput <- reactive({
-    switch(input$dataset,
-           "imdb" = movies[,c(2,16)],
-           "meta" = movies[,c(2,15)])
-  })
   
   number <- reactive({
     input$movienumber
@@ -52,6 +73,30 @@ shinyServer(function(input, output) {
            "Year released" = "Year")
   })
   
+  bubblesize <- reactive({
+    switch(input$bubblesize,
+           "No. of IMDB votes" = "imdbVotes",
+           "No. of Rotten Tomatoes ratings" = "tomatoUserReviews",
+           "Metascore" = "Metascore")
+  })
+  
+  colorv <- reactive({
+    switch(input$colorv,
+           "MPAA rating" = "Rated",
+           "Year released" = "Year",
+           "Decade released" = "decade"
+    )
+  })
+  
+  boxxaxis <- reactive({
+    switch(input$boxxaxis,
+           "IMDB rating" = 16, 
+           "Rotten Tomatoes user rating" = 28, 
+           "Rotten Tomatoes rating" = 22, 
+           "Tomatometer" = 20, 
+           "Year released" = 2)
+  })
+  
   output$view <- renderGvis({
     # only the selected amount of movies
     data <- movies[1:number(),]
@@ -60,9 +105,13 @@ shinyServer(function(input, output) {
                     idvar="Title",
                     xvar=xaxis(),
                     yvar=yaxis(),
-                    sizevar="V35",
-                    colorvar="Rated",
+                    sizevar=bubblesize(),
+                    colorvar=colorv(),
                     options=list(height=950,
                                  fontSize=10))
+  })
+  
+  output$boxofficeview <- renderGvis({
+    gvisScatterChart(movieswithBO[,c(boxxaxis(), 35, 36)], options=list(height=950,fontSize=10))
   })
 })
--- ui.R
+++ ui.R
@@ -1,30 +1,50 @@
 library(shiny)
 library(ggvis)
 
-shinyUI(fluidPage(
-  titlePanel("asdasdasd"),
-  fluidRow(
+shinyUI(
+  navbarPage("Movie charts",
+
+  tabPanel("Ratings",
+    fluidRow(
     
-    column(3,
-           wellPanel(
-             h4("Filters"),
-             sliderInput("movienumber", "Number of movies", 30, 250, 200)
-           ),
-           wellPanel(
-             h4("Axes"),
-             selectInput("xaxis", "Choose X axis:", 
-                         choices = c("IMDB rating", "Rotten Tomatoes user rating", "Rotten Tomatoes rating", "Tomatometer", "Year released")),
-             selectInput("yaxis", "Choose Y axis:", 
-                         choices = c("Rotten Tomatoes rating", "Rotten Tomatoes user rating", "Tomatometer", "Year released", "IMDB rating")),
-             selectInput("bubblesize", "Choose bubble size", 
-                         choices = c("No. of IMDB votes", "No. of Rotten Tomatoes ratings", "Metascore")),
-             selectInput("color", "Choose coloring methpd", 
-                         choices = c("MPAA rating", "Year released"))
-           )
-    ),
-    
-    column(9,
-           htmlOutput("view")
+      column(3,
+             wellPanel(
+               h4("Filters"),
+               sliderInput("movienumber", "Number of movies", 30, 250, 200)
+             ),
+             wellPanel(
+               h4("Axes"),
+               selectInput("xaxis", "Choose X axis:", 
+                           choices = c("IMDB rating", "Rotten Tomatoes user rating", "Rotten Tomatoes rating", "Tomatometer", "Year released")),
+               selectInput("yaxis", "Choose Y axis:", 
+                           choices = c("Rotten Tomatoes rating", "Rotten Tomatoes user rating", "Tomatometer", "Year released", "IMDB rating")),
+               selectInput("bubblesize", "Choose bubble size", 
+                           choices = c("No. of IMDB votes", "No. of Rotten Tomatoes ratings", "Metascore")),
+               selectInput("colorv", "Choose coloring method", 
+                           choices = c("MPAA rating", "Year released", "Decade released"))
+             )
+      ),
+      
+      column(9,
+             htmlOutput("view")
+      )
+    )),
+  
+    tabPanel("Box office",
+       fluidRow(
+         
+         column(3,
+                wellPanel(
+                  h4("Filters"),
+                  selectInput("boxxaxis", "Choose X axis:", 
+                              choices = c("IMDB rating", "Rotten Tomatoes user rating", "Rotten Tomatoes rating", "Tomatometer", "Year released"))
+                )
+         ),
+         
+         column(9,
+                htmlOutput("boxofficeview")
+         )
+       )
     )
   )
-))
+)
